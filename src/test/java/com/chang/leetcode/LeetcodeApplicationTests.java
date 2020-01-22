784af0b105b670ae4727352e7afa7a0eda8c3554
batterage <2389889598@qq.com>
2020-01-22 13:12:23 +0800

    Initial commit

---
+++ src/test/java/com/chang/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chang.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
